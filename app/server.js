513369212b3273f5dd073c7a5534c21a1c7002f7
Danny Flores <dnnfr_10@hotmail.com>
2026-08-21 11:13:38 +1000

    feat: add initial application

---
+++ app/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from DevOps Platform!');
+});
+
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'Up',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Aplication running on port ${PORT}`);
+});
